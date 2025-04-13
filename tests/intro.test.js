6be450685b451bebd6c15cdca99bd5716d6993b1
Ali Muhammad <81983704+Ali-muhammed7040@users.noreply.github.com>
2025-04-14 00:12:08 +0500

    unit testing

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from 'vitest'
-import { max } from '../src/intro';
+import { fizzBuzz, max } from '../src/intro';
 
 describe(' max ', () => {
     it('should return the first argument if it is greater than the second', () => {
@@ -11,5 +11,21 @@ describe(' max ', () => {
 
     it('should return the first argument if it is argument are equal', () => {
         expect(max(5, 5)).toBe(5);
+    })
+})
+
+describe(' fizzBuzz ', () => {
+    it('should return FizzBuzz if number is divisble by 3 and 5 ', () => {
+        expect(fizzBuzz(15)).toBe('FizzBuzz');
+    })
+    it('should return Fizz if number is divisble by 3 ', () => {
+        expect(fizzBuzz(9)).toBe('Fizz');
+    })
+    it('should return Buzz if number is divisble by 5 ', () => {
+        expect(fizzBuzz(10)).toBe('Buzz');
+    })
+
+    it('if not divisible by any of these it should return string', () => {
+        expect(fizzBuzz(17)).toBe(17..toString());
     })
 })
